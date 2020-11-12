983d00f48c354e9987870c91036eb20821e04d9f
Daniel Schulz <faulolio@gmail.com>
2020-11-12 19:37:26 +0100

    Add Testat02

---
+++ unity-pp/tests/Testat02/Assets/Tests/TestUtils.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Tests
+{
+    public class TestUtils
+    {
+        public static bool Approximately(Vector3 one, Vector3 two)
+        {
+            return Mathf.Approximately(one.x, two.x)
+                && Mathf.Approximately(one.y, two.y)
+                && Mathf.Approximately(one.z, two.z);
+        }
+    }
+}
