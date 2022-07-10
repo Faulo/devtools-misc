3db0f94a457857a22836f1295ffea1cba5aaea8f
Daniel Schulz <faulolio@gmail.com>
2022-07-10 15:50:22 +0200

    Remove unused code

--- src/Slothsoft/Devtools/Update/Fix/FixDocsCreate.php
+++ src/Slothsoft/Devtools/Update/Fix/FixDocsCreate.php
@@ -7,9 +7,6 @@ use Slothsoft\Devtools\Update\UpdateInterface;
 class FixDocsCreate implements UpdateInterface {
 
     public function runOn(array $project) {
-        $version = 'latest';
-        // $version = '2.9.0';
-        // $version = '3.0.0-alpha1';
         if (is_file($project['workspaceDir'] . 'phpdoc.dist.xml')) {
             CLI::execute('phpDocumentor run');
         }
