08e5aba098128ec3e9554afc0ca5c99cb8c9c1c2
Daniel Schulz <faulolio@gmail.com>
2024-03-31 00:29:16 +0100

    feat: fix aminet

--- scripts/aminet.php
+++ scripts/aminet.php
@@ -1,13 +1,19 @@
 <?php
+declare(strict_types = 1);
+namespace Slothsoft\Devtools\Misc;
 
 use Slothsoft\Core\Storage;
 use Slothsoft\Core\Calendar\Seconds;
 use Slothsoft\Core\DOMHelper;
-
-require '../vendor/autoload.php';
-require '../../../global/slothsoft.core.php';
-require '../../../global/slothsoft.core.xslt.php';
-require '../../../global/slothsoft.core.dbms.php';
+foreach ([
+    __DIR__ . '/../../../autoload.php',
+    __DIR__ . '/../vendor/autoload.php'
+] as $file) {
+    if (file_exists($file)) {
+        require_once $file;
+        break;
+    }
+}
 
 $paths = [];
 $paths['game/2play'] = 'Aminet/Multiplayer';
@@ -22,10 +28,19 @@ $paths['game/text'] = 'Aminet/Text';
 
 $blacklist = [];
 $blacklist[] = 'TwSrc';
- 
+
 const DIR_DOWNLOADS = 'Aminet/Downloads/';
+
+const SEVENZIP = 'C:\\Program Files\\7-Zip\\7z.exe';
+
 const AMINET_DOMAIN = 'http://aminet.net';
+
 const AMINET_SEARCH = '/search?path[]=%s&arch[]=m68k-amigaos&start=%d';
+
+if (! is_dir(DIR_DOWNLOADS)) {
+    mkdir(DIR_DOWNLOADS, 0777, true);
+    copy(__DIR__ . DIRECTORY_SEPARATOR . 'folder.info', DIR_DOWNLOADS . '.info');
+}
 
 foreach ($paths as $path => $folder) {
     $packages = [];
@@ -33,9 +48,9 @@ foreach ($paths as $path => $folder) {
         $uri = sprintf(AMINET_DOMAIN . AMINET_SEARCH, $path, $i);
         $document = Storage::loadExternalDocument($uri, Seconds::MONTH);
         $xpath = DOMHelper::loadXPath($document);
-        
+
         $nodes = $xpath->evaluate('//*[@id="start_listing"]/*[@class]');
-        if (!$nodes->length) {
+        if (! $nodes->length) {
             break;
         }
         foreach ($nodes as $node) {
@@ -46,20 +61,20 @@ foreach ($paths as $path => $folder) {
             $package['name'] = pathinfo($package['file'], PATHINFO_FILENAME);
             $package['extension'] = pathinfo($package['file'], PATHINFO_EXTENSION);
             $package['folder'] = $folder . DIRECTORY_SEPARATOR . $package['name'];
-            
+
             $isDemo = stripos($package['description'], 'demo') != false;
             $isWin = ($package['extension'] === 'zip' or $package['extension'] === 'exe');
             $isBlack = in_array($package['name'], $blacklist);
-            
+
             $package['skip'] = ($isDemo or $isWin or $isBlack);
             $packages[] = $package;
         }
     }
     if ($packages) {
         echo $folder . PHP_EOL;
-        if (!is_dir($folder)) {
+        if (! is_dir($folder)) {
             mkdir($folder, 0777, true);
-            copy('folder.info', $folder . '.info');
+            copy(__DIR__ . DIRECTORY_SEPARATOR . 'folder.info', $folder . '.info');
         }
         foreach ($packages as $package) {
             echo $package['name'];
@@ -74,15 +89,15 @@ foreach ($paths as $path => $folder) {
             } else {
                 if (file_exists($package['folder'] . '.info')) {
                     echo '...OK';
-                    if (!file_exists($package['folder'])) {
+                    if (! file_exists($package['folder'])) {
                         unlink($package['folder'] . '.info');
                     }
                 } else {
                     echo '...DOWNLOAD';
-                    if (!file_exists($package['file'])) {
+                    if (! file_exists($package['file'])) {
                         file_put_contents($package['file'], file_get_contents($package['href']));
                     }
-                    $command = sprintf('7z x %s -o%s', escapeshellarg($package['file']), escapeshellarg($package['folder']));
+                    $command = sprintf('%s x %s -o%s', escapeshellarg(SEVENZIP), escapeshellarg($package['file']), escapeshellarg($package['folder']));
                     echo '  ' . $command . PHP_EOL;
                     echo `$command`;
                     if (file_exists($package['folder'])) {
