8d6dd2c236ef18292a30479fcbe7f3384455a35c
Daniel Schulz <faulolio@gmail.com>
2024-04-01 11:05:31 +0200

    feat: add unity

--- config/modules.php
+++ config/modules.php
@@ -4,285 +4,76 @@ declare(strict_types = 1);
 use Slothsoft\Devtools\Misc\Update\ModuleManager;
 use Slothsoft\Devtools\Misc\Update\ProjectDatabase;
 
-$modules = array(
-    0 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'amber',
-        'composerId' => 'slothsoft/amber',
-        'farahId' => 'farah://slothsoft@amber',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-amber',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/amber',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/amber'
-    ),
-    1 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'chat',
-        'composerId' => 'slothsoft/chat',
-        'farahId' => 'farah://slothsoft@chat',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-chat',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/chat',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/chat'
-    ),
-    2 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'comics',
-        'composerId' => 'slothsoft/comics',
-        'farahId' => 'farah://slothsoft@comics',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-comics',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/comics',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/comics'
-    ),
-    3 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'core',
-        'composerId' => 'slothsoft/core',
-        'farahId' => 'farah://slothsoft@core',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-core',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/core',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/core'
-        // 'standalone' => true,
-    ),
-    4 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'cron',
-        'composerId' => 'slothsoft/cron',
-        'farahId' => 'farah://slothsoft@cron',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-cron',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/cron',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/cron'
-    ),
-    // 5 =>
-    // array (
-    // 'vendor' => 'slothsoft',
-    // 'name' => 'daniel',
-    // 'composerId' => 'slothsoft/daniel',
-    // 'farahId' => 'farah://slothsoft@daniel',
-    // 'githubUrl' => 'https://github.com/Faulo/slothsoft-daniel',
-    // 'homeUrl' => 'http://farah.slothsoft.net/modules/daniel',
-    // 'packagistUrl' => 'https://packagist.org/packages/slothsoft/daniel',
-    // ),
-    /*
-     * 6 =>
-     * array (
-     * 'vendor' => 'slothsoft',
-     * 'name' => 'dbms',
-     * 'composerId' => 'slothsoft/dbms',
-     * 'farahId' => 'farah://slothsoft@dbms',
-     * 'githubUrl' => 'https://github.com/Faulo/slothsoft-dbms',
-     * 'homeUrl' => 'http://farah.slothsoft.net/modules/dbms',
-     * 'packagistUrl' => 'https://packagist.org/packages/slothsoft/dbms',
-     * 'standalone' => true,
-     * ),
-     * //
-     */
-    // 7 =>
-    // array (
-    // 'vendor' => 'slothsoft',
-    // 'name' => 'dev',
-    // 'composerId' => 'slothsoft/dev',
-    // 'farahId' => 'farah://slothsoft@dev',
-    // 'githubUrl' => 'https://github.com/Faulo/slothsoft-dev',
-    // 'homeUrl' => 'http://farah.slothsoft.net/modules/dev',
-    // 'packagistUrl' => 'https://packagist.org/packages/slothsoft/dev',
-    // ),
-    8 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'dragonage',
-        'composerId' => 'slothsoft/dragonage',
-        'farahId' => 'farah://slothsoft@dragonage',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-dragonage',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/dragonage',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/dragonage'
-    ),
-    
-    /*
-  9 => 
-  array (
-    'vendor' => 'slothsoft',
-    'name' => 'extensions',
-    'composerId' => 'slothsoft/extensions',
-    'farahId' => 'farah://slothsoft@extensions',
-    'githubUrl' => 'https://github.com/Faulo/slothsoft-extensions',
-    'homeUrl' => 'http://farah.slothsoft.net/modules/extensions',
-    'packagistUrl' => 'https://packagist.org/packages/slothsoft/extensions',
-  ),
-  //*/
-  10 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'farah',
-        'composerId' => 'slothsoft/farah',
-        'farahId' => 'farah://slothsoft@farah',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-farah',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/farah',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/farah'
-    ),
-    11 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'fireemblem',
-        'composerId' => 'slothsoft/fireemblem',
-        'farahId' => 'farah://slothsoft@fireemblem',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-fireemblem',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/fireemblem',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/fireemblem'
-    ),
-    12 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'lang',
-        'composerId' => 'slothsoft/lang',
-        'farahId' => 'farah://slothsoft@lang',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-lang',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/lang',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/lang'
-    ),
-    13 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'minecraft',
-        'composerId' => 'slothsoft/minecraft',
-        'farahId' => 'farah://slothsoft@minecraft',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-minecraft',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/minecraft',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/minecraft'
-    ),
-    14 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'mtg',
-        'composerId' => 'slothsoft/mtg',
-        'farahId' => 'farah://slothsoft@mtg',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-mtg',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/mtg',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/mtg'
-    ),
-    15 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'pokemon',
-        'composerId' => 'slothsoft/pokemon',
-        'farahId' => 'farah://slothsoft@pokemon',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-pokemon',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/pokemon',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/pokemon'
-    ),
-  /*
-  16 => 
-  array (
-    'vendor' => 'slothsoft',
-    'name' => 'pt',
-    'composerId' => 'slothsoft/pt',
-    'farahId' => 'farah://slothsoft@pt',
-    'githubUrl' => 'https://github.com/Faulo/slothsoft-pt',
-    'homeUrl' => 'http://farah.slothsoft.net/modules/pt',
-    'packagistUrl' => 'https://packagist.org/packages/slothsoft/pt',
-  ),
-  //*/
-  17 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'savegame',
-        'composerId' => 'slothsoft/savegame',
-        'farahId' => 'farah://slothsoft@savegame',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-savegame',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/savegame',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/savegame'
-    ),
-    18 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'schema',
-        'composerId' => 'slothsoft/schema',
-        'farahId' => 'farah://slothsoft@schema',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-schema',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/schema',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/schema'
-    ),
-    // 19 =>
-    // array (
-    // 'vendor' => 'slothsoft',
-    // 'name' => 'slothsoft',
-    // 'composerId' => 'slothsoft/slothsoft',
-    // 'farahId' => 'farah://slothsoft@slothsoft',
-    // 'githubUrl' => 'https://github.com/Faulo/slothsoft-slothsoft',
-    // 'homeUrl' => 'http://farah.slothsoft.net/modules/slothsoft',
-    // 'packagistUrl' => 'https://packagist.org/packages/slothsoft/slothsoft',
-    // ),
-    20 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'sse',
-        'composerId' => 'slothsoft/sse',
-        'farahId' => 'farah://slothsoft@sse',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-sse',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/sse',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/sse'
-    ),
-    21 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'talesof',
-        'composerId' => 'slothsoft/talesof',
-        'farahId' => 'farah://slothsoft@talesof',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-talesof',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/talesof',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/talesof'
-    ),
-    22 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'tetris',
-        'composerId' => 'slothsoft/tetris',
-        'farahId' => 'farah://slothsoft@tetris',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-tetris',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/tetris',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/tetris'
-    ),
-    23 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'therapy',
-        'composerId' => 'slothsoft/therapy',
-        'farahId' => 'farah://slothsoft@therapy',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-therapy',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/therapy',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/therapy'
-    ),
-    24 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'twitter',
-        'composerId' => 'slothsoft/twitter',
-        'farahId' => 'farah://slothsoft@twitter',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-twitter',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/twitter',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/twitter'
-    ),
-    25 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'w3c',
-        'composerId' => 'slothsoft/w3c',
-        'farahId' => 'farah://slothsoft@w3c',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-w3c',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/w3c',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/w3c'
-        // 'standalone' => true,
-    ),
-    26 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'webrtc',
-        'composerId' => 'slothsoft/webrtc',
-        'farahId' => 'farah://slothsoft@webrtc',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-webrtc',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/webrtc',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/webrtc'
-    ),
-    27 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'whatthehell',
-        'composerId' => 'slothsoft/whatthehell',
-        'farahId' => 'farah://slothsoft@whatthehell',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-whatthehell',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/whatthehell',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/whatthehell'
-    ),
-    28 => array(
-        'vendor' => 'slothsoft',
-        'name' => 'blob',
-        'composerId' => 'slothsoft/blob',
-        'farahId' => 'farah://slothsoft@blob',
-        'githubUrl' => 'https://github.com/Faulo/slothsoft-blob',
-        'homeUrl' => 'http://farah.slothsoft.net/modules/blob',
-        'packagistUrl' => 'https://packagist.org/packages/slothsoft/blob'
-    )
-);
+$modules = [
+    [
+        'name' => 'amber'
+    ],
+    [
+        'name' => 'blob'
+    ],
+    [
+        'name' => 'chat'
+    ],
+    [
+        'name' => 'comics'
+    ],
+    [
+        'name' => 'core'
+    ],
+    [
+        'name' => 'cron'
+    ],
+    [
+        'name' => 'dragonage'
+    ],
+    [
+        'name' => 'farah'
+    ],
+    [
+        'name' => 'fireemblem'
+    ],
+    [
+        'name' => 'lang'
+    ],
+    [
+        'name' => 'minecraft'
+    ],
+    [
+        'name' => 'mtg'
+    ],
+    [
+        'name' => 'pokemon'
+    ],
+    [
+        'name' => 'talesof'
+    ],
+    [
+        'name' => 'savegame'
+    ],
+    [
+        'name' => 'schema'
+    ],
+    [
+        'name' => 'sse'
+    ],
+    [
+        'name' => 'tetris'
+    ],
+    [
+        'name' => 'therapy'
+    ],
+    [
+        'name' => 'twitter'
+    ],
+    [
+        'name' => 'unity'
+    ],
+    [
+        'name' => 'w3c'
+    ],
+    [
+        'name' => 'webrtc'
+    ]
+];
 
 ProjectDatabase::instance()->groups[] = new ModuleManager('module', 'R:\\Eclipse\\workspace', $modules);
--- src/Update/ModuleManager.php
+++ src/Update/ModuleManager.php
@@ -4,24 +4,14 @@ namespace Slothsoft\Devtools\Misc\Update;
 
 class ModuleManager extends PHPProjectManager {
 
-    public static function createSlothsoftModules(...$names): array {
-        $modules = [];
-        foreach ($names as $name) {
-            $modules[] = [
-                'vendor' => 'slothsoft',
-                'name' => $name,
-                'composerId' => "slothsoft/$name",
-                'farahId' => "farah://slothsoft@$name",
-                'githubUrl' => "https://github.com/Faulo/slothsoft-$name",
-                'homeUrl' => "http://farah.slothsoft.net/modules/$name",
-                'packagistUrl' => "https://packagist.org/packages/slothsoft/$name"
-            ];
-        }
-        return $modules;
-    }
-
     protected function loadProject(array &$module) {
-        $module['workspaceId'] = "$module[vendor]-$module[name]";
+        $module['vendor'] ??= 'slothsoft';
+        $module['composerId'] ??= "$module[vendor]/$module[name]";
+        $module['farahId'] ??= "farah://$module[vendor]@$module[name]";
+        $module['githubUrl'] ??= "https://github.com/Faulo/$module[vendor]-$module[name]";
+        $module['homeUrl'] ??= "http://farah.slothsoft.net/modules/$module[name]";
+        $module['packagistUrl'] ??= "https://packagist.org/packages/$module[composerId]";
+        $module['workspaceId'] ??= "$module[vendor]-$module[name]";
     }
 }
 
