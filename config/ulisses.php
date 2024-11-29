ce07b7f8db1f592ea6b1e6d63b20f0d979213cfc
Daniel Schulz <faulolio@gmail.com>
2024-11-29 15:47:56 +0100

    fix: update utilities

--- config/ulisses.php
+++ config/ulisses.php
@@ -181,7 +181,7 @@ $projectManifestForbidden = [
     "de.ulisses-spiele.hexxen1733.animations"
 ];
 $packageManifestDependencies = [
-    "de.ulisses-spiele.core.utilities" => "6.1.0",
+    "de.ulisses-spiele.core.utilities" => "6.1.1",
     "com.unity.test-framework" => "2.0.1-exp.2",
     // "jp.magicasoft.magicacloth" => "1.0.0",
     "net.tnrd.nsubstitute" => "5.1.0"
